8c05c2192a6f00e0ad128ac98b070eaee1cfc68f
Katalina Okano <git@kat.cafe>
2022-02-03 21:07:20 -0500

    fresh project base

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "remix";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <Link to="/posts">Posts</Link>
+    </div>
+  );
+}
